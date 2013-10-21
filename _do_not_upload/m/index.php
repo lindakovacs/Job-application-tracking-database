335cf3d14cfb428fe8b60dcb668d07eb0fae670c
Mark E. Taylor <mark.e.taylor@icloud.com>
2013-10-21 16:12:33 +0100

    removed unused code

---
+++ _do_not_upload/m/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+
+<meta name="keywords" content="WORD, WORD" />
+<meta name="description" content="WORD, WORD" />
+<meta name="author" content="Mark E Taylor" />
+<meta name="copyright" content="&copy; 2000-<?php date_default_timezone_set('Europe/London'); echo date('Y');?> Mark E Taylor" />
+
+<meta name="verify-v1" content="UljtH7LG3ckJ7GA201Lp5RkN/e+OeG9owhnmzmfYhos=" />
+
+<link href="data/feed_NAME.xml" rel="alternate" type="application/rss+xml" title="TITLE" />
+
+<link href="css/style.css"  rel="stylesheet" media="screen" type="text/css" />
+<!--
+<link href="css/forms.css" rel="stylesheet" media="screen" type="text/css" />
+<link href="css/table.css" rel="stylesheet" media="screen" type="text/css" />
+<link href="css/print.css" rel="stylesheet" media="print" type="text/css" />
+-->
+
+<!-- The jQuery library. -->
+<!-- <script src="http://code.jquery.com/jquery-latest.js"></script> -->
+
+<title>Test page for mobile</title>
+
+</head>
+
+<body>
+<h3>This is the mobile version of the web site</h3>
+</body>
+
+<!-- The script that contains my Javascripy/jQuery code. -->
+<!-- <script src="js/canvas.js" type="application/javascript"></script> -->
+</html>
